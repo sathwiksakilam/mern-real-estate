3dec4fa274aa255289ad588e842e8281637ca002
Your Name <you@example.com>
2024-07-10 23:58:08 +0530

    create sign in api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,14 +1,42 @@
 import User from "../models/user.model.js";
 import bcrypt from "bcryptjs";
 import { errorHandler } from "../utils/error.js";
+import jwt from "jsonwebtoken";
 
-export const signup = async (req, res,next) => {
+export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
   const hashedPassword = bcrypt.hashSync(password, 10); //10 is salt number which is the number of rounds of creating salts
   const newUser = new User({ username, email, password: hashedPassword });
   try {
     await newUser.save();
     res.status(201).json("User created successfully");
+  } catch (err) {
+    next(err);
+  }
+};
+
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+  try {
+    const validUser = await User.findOne({ email });
+    if (!validUser) {
+      return next(errorHandler(404, "User not found!"));
+    }
+    const validPassword = bcrypt.compareSync(password, validUser.password);
+    if (!validPassword) {
+      return next(errorHandler(401, "Wrong Credentials!"));
+    }
+    const {password:pass,...rest} = validUser._doc;
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET, {
+      expiresIn: "1d", // Expires in 1 day
+    });
+    res
+      .cookie("access_token", token, {
+        httpOnly: true,
+        expires: new Date(Date.now() + 24 * 60 * 60 * 1000),
+      })
+      .status(200)
+      .json(rest);
   } catch (err) {
     next(err);
   }
