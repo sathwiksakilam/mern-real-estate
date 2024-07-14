23285838a63fd73bb3ba78017990c0764386a788
Your Name <you@example.com>
2024-07-14 19:08:05 +0530

    complete image upload functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,40 +1,120 @@
-import React from "react";
+import React, { useEffect, useState, useRef } from "react";
 import { useSelector } from "react-redux";
+import {
+  getDownloadURL,
+  getStorage,
+  ref,
+  uploadBytesResumable,
+} from "firebase/storage";
+import { app } from "../firebase.js";
 
 export default function Profile() {
+  const fileRef = useRef(null);
   const { currentUser } = useSelector((state) => state.user);
-  // console.log(currentUser.avatar)
+  const [file, setFile] = useState(undefined);
+  const [filePerc, setFilePerc] = useState(0);
+  const [fileUploadError, setFileUploadError] = useState(false);
+  const [formData, setFormData] = useState({
+    username: "",
+    email: "",
+    password: "",
+    avatar: currentUser?.avatar || "default-avatar-url",
+  });
+  // console.log(formData);
+  // console.log(filePerc);
+
+  useEffect(() => {
+    if (file) {
+      handleFileUpload(file);
+    }
+  }, [file]);
+
+  const handleFileUpload = (file) => {
+    if (!file.type.startsWith("image/")) {
+      setFileUploadError(true);
+      return;
+    }
+
+    const storage = getStorage(app);
+    const fileName = new Date().getTime() + file.name;
+    const storageRef = ref(storage, fileName);
+    const uploadTask = uploadBytesResumable(storageRef, file);
+
+    uploadTask.on(
+      "state_changed",
+      (snapshot) => {
+        const progress =
+          (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        setFilePerc(Math.round(progress));
+      },
+      (error) => {
+        setFileUploadError(true);
+      },
+      () => {
+        getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+          setFormData({ ...formData, avatar: downloadURL });
+        });
+      }
+    );
+  };
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value });
+  };
+
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl font-semibold text-center my-7">Profile</h1>
       <form className="flex flex-col gap-4">
+        <input
+          onChange={(e) => setFile(e.target.files[0])}
+          type="file"
+          ref={fileRef}
+          hidden
+          accept="image/*"
+        ></input>
         <img
-          src={currentUser.avatar}
+          onClick={() => fileRef.current.click()}
+          src={formData.avatar}
           alt="profile"
           className="rounded-full h-24 w-24 object-cover cursor-pointer self-center mt-2"
         ></img>
+        <p className="text-sm self-center">
+          {fileUploadError ? (
+            <span className="text-red-700">Error Image Upload</span>
+          ) : filePerc > 0 && filePerc < 100 ? (
+            <span className="text-slate-700">{`Uploading ${filePerc}`}</span>
+          ) : filePerc === 100 ? (
+            <span className="text-green-700">Image successfully uploaded!</span>
+          ) : (
+            <span></span>
+          )}
+        </p>
         <input
           type="text"
           placeholder="username"
           className="border p-3 rounded-lg"
           id="username"
+          value={formData.username}
+          onChange={handleChange}
         ></input>
         <input
           type="email"
           placeholder="email"
           className="border p-3 rounded-lg"
           id="email"
+          value={formData.email}
+          onChange={handleChange}
         ></input>
         <input
           type="password"
           placeholder="password"
           className="border p-3 rounded-lg"
           id="password"
+          value={formData.password}
+          onChange={handleChange}
         ></input>
-        <button
-          className="bg-slate-700 text-white rounded-lg p-3 uppercase hover:opacity-95
-        disabled:opacity-80"
-        >
+        <button className="bg-slate-700 text-white rounded-lg p-3 uppercase hover:opacity-95 disabled:opacity-80">
           Update
         </button>
       </form>
