8ddd81795c53dc3f00d900311480326eda035da2
Your Name <you@example.com>
2024-07-19 11:20:09 +0530

    create get user listings api route

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,6 +1,8 @@
 import { errorHandler } from "../utils/error.js";
 import bcryptjs from 'bcryptjs'
 import User from '../models/user.model.js'
+import Listing from "../models/listing.model.js";
+
 export const test = (req, res) => {
   res.json({
     message: "Hello World!",
@@ -51,4 +53,22 @@ export const deleteUser = async(req,res,next) =>{
     {
         next(error);
     }
+}
+
+
+export const getUserListings = async(req,res,next)=>{
+  if(req.user.id=== req.params.id)
+  {
+    try{
+      const listings = await Listing.find({userRef:req.params.id});
+      res.status(200).json(listings);
+    } 
+    catch(err)
+    {
+      next(err);
+    }
+  }
+  else{
+    return next(errorHandler(401,'You can only view your own listings'))
+  }
 }
