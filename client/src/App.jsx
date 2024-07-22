b0a5ec8620e31620123688fbdddef48ea9b8154b
Your Name <you@example.com>
2024-07-23 01:58:36 +0530

    updated search page ui and add onchange and onsubmit functionality to the search page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,6 +10,7 @@ import PrivateRoute from "./components/PrivateRoute";
 import CreateListing from "./pages/CreateListing";
 import UpdateListing from './pages/UpdateListing';
 import OtherListing from "./pages/OtherListing";
+import Search from "./pages/Search";
 
 export default function App() {
   return (
@@ -21,6 +22,7 @@ export default function App() {
         <Route path="/sign-up" element={<SignUp />}></Route>
         <Route path="/about" element={<About />}></Route>
         <Route path="/listing/:listingId" element={<OtherListing />}></Route>
+        <Route path="/search" element={<Search/>}></Route>
         <Route element={<PrivateRoute />}>
           <Route path="/profile" element={<Profile />}></Route>
           <Route path="/create-listing" element={<CreateListing />}></Route>
